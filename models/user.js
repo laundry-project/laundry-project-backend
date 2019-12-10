5e8ac8cfdd7df6801283641b01e8194f67b8d8c1
robyafrizal <robyafrizal86@gmail.com>
2019-12-10 15:00:55 +0700

    Add User

--- models/user.js
+++ models/user.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  telephone: Number
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
