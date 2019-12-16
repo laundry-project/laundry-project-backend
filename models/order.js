f3f2fec3670a9bc5d96bc32dc6c03c1df55acdd0
taufik66645 <taufik.66645@gmail.com>
2019-12-16 11:29:48 +0700

    edit order

--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   user: String,
-  address: String,
+  service: [String],
   date: String,
   amount: Number,
   price: Number,
